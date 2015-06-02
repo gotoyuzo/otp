a072e948a95b4916654865aaa8d37887ed55e7d6
Yuuzou Gotou <gotoyuzo@notwork.org>
2015-06-02 18:37:16 +0900

    refine test code

--- test/test_base32.rb
+++ test/test_base32.rb
@@ -2,26 +2,28 @@ require "test/unit"
 require "otp/base32"
 
 class TestBase32 < Test::Unit::TestCase
-  def test_encode
-   assert_equal(::OTP::Base32.encode(""), "")
-   assert_equal(::OTP::Base32.encode("f"), "MY======")
-   assert_equal(::OTP::Base32.encode("fo"), "MZXQ====")
-   assert_equal(::OTP::Base32.encode("foo"), "MZXW6===")
-   assert_equal(::OTP::Base32.encode("foob"), "MZXW6YQ=")
-   assert_equal(::OTP::Base32.encode("fooba"), "MZXW6YTB")
-   assert_equal(::OTP::Base32.encode("foobar"), "MZXW6YTBOI======")
-   assert_equal(::OTP::Base32.encode("\u{3042}\u{3044}\u{3046}\u{3048}\u{304a}"), "4OAYFY4BQTRYDBXDQGEOHAMK")
+  def assert_encode(plain, encoded)
+     assert_equal(::OTP::Base32.encode(plain), encoded)
   end
 
-  def test_decode
-   assert_equal(::OTP::Base32.decode(""), "")
-   assert_equal(::OTP::Base32.decode("MY======"), "f")
-   assert_equal(::OTP::Base32.decode("MZXQ===="), "fo")
-   assert_equal(::OTP::Base32.decode("MZXW6==="), "foo")
-   assert_equal(::OTP::Base32.decode("MZXW6YQ="), "foob")
-   assert_equal(::OTP::Base32.decode("MZXW6YTB"), "fooba")
-   assert_equal(::OTP::Base32.decode("MZXW6YTBOI======"), "foobar")
-   assert_equal(::OTP::Base32.decode("mzxw6ytboi======"), "foobar")
-   assert_equal(::OTP::Base32.decode("4OAYFY4BQTRYDBXDQGEOHAMK"), "\u{3042}\u{3044}\u{3046}\u{3048}\u{304a}".force_encoding("binary"))
+  def assert_decode(encoded, plain)
+     assert_equal(::OTP::Base32.decode(encoded), plain)
+  end
+
+  def assert_encode_decode(plain, encoded)
+    plain.force_encoding("binary")
+    assert_encode(plain, encoded)
+    assert_decode(encoded, plain)
+  end
+
+  def test_base32
+    assert_encode_decode("", "")
+    assert_encode_decode("f", "MY======")
+    assert_encode_decode("fo", "MZXQ====")
+    assert_encode_decode("foo", "MZXW6===")
+    assert_encode_decode("foob", "MZXW6YQ=")
+    assert_encode_decode("fooba", "MZXW6YTB")
+    assert_encode_decode("foobar", "MZXW6YTBOI======")
+    assert_encode_decode("\u{3042}\u{3044}\u{3046}\u{3048}\u{304a}", "4OAYFY4BQTRYDBXDQGEOHAMK")
   end
 end
