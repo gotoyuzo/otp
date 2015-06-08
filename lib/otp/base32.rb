6fc55390b87f37441f966682866167dc4566f6ae
Yuuzou Gotou <gotoyuzo@notwork.org>
2015-06-08 13:11:58 +0900

    Solve irregular cases

--- lib/otp/base32.rb
+++ lib/otp/base32.rb
@@ -1,11 +1,11 @@
 module OTP
   module Base32
-    BASE32_ENCODE = %w(
+    ENCODE_CHARS = %w(
       A B C D E F G H I J K L M N O P
       Q R S T U V W X Y Z 2 3 4 5 6 7
     )
 
-    BASE32_DECODE = {
+    DECODE_MAP = {
       ?A=>0,  ?B=>1,  ?C=>2,  ?D=>3,  ?E=>4,  ?F=>5,  ?G=>6,  ?H=>7, 
       ?I=>8,  ?J=>9,  ?K=>10, ?L=>11, ?M=>12, ?N=>13, ?O=>14, ?P=>15,
       ?Q=>16, ?R=>17, ?S=>18, ?T=>19, ?U=>20, ?V=>21, ?W=>22, ?X=>23, 
@@ -13,23 +13,35 @@ module OTP
       ?==>-1,
     }
 
+    DECODE_LENGTH = {
+      1 => 1,  #  5 bits -> 1 byte (irregular)
+      2 => 1,  # 10 bits -> 1 byte
+      3 => 2,  # 15 bits -> 2 bytes (irregular)
+      4 => 2,  # 20 bits -> 2 bytes
+      5 => 3,  # 25 bits -> 3 bytes
+      6 => 4,  # 30 bits -> 4 bytes (irregular)
+      7 => 4,  # 35 bits -> 4 bytes
+      8 => 5,  # 40 bits -> 5 bytes
+    }
+
     module_function
 
-    def encode(bytes)
+    def encode(bytes, padding: true)
       return nil unless bytes
+      pad = padding ? ?= : ""
       ret = ""
       bytes = bytes.dup.force_encoding("binary")
       off = 0
       while off < bytes.length
         n = 0
         bits = bytes[off, 5]
+        off += 5
         l = (bits.length * 8.0 / 5.0).ceil
         bits << "\0" while bits.length < 5
         bits.each_byte{|b| n = (n << 8) | b }
         (1..8).each do |i|
-          ret << ((i > l) ? ?= : BASE32_ENCODE[(n >> (8-i)*5) & 0x1f])
+          ret << ((i > l) ? pad : ENCODE_CHARS[(n >> (8-i)*5) & 0x1f])
         end
-        off += 5
       end
       return ret
     end
@@ -43,20 +55,24 @@ module OTP
       while off < chars.length
         n = l = 0
         bits = chars[off, 8]
+        off += 8
         bits << "=" while bits.length < 8
         bits.each_char.with_index do |c, i|
-          d = BASE32_DECODE[c]
-          raise ArgumentError, "invalid char: #{c}" unless d
-          if d < 0
+          d = DECODE_MAP[c]
+          if d.nil?
+            raise ArgumentError, "invalid char: #{c}"
+          elsif d < 0
             n <<= 5 * (8-i)
+            off = chars.length
             break
+          else
+            n <<= 5
+            n |= d
+            l = ((i+1) * 5.0 / 8.0).floor
+            l = DECODE_LENGTH[i+1]
           end
-          n <<= 5
-          n |= d
-          l = ((i+1) * 5.0 / 8.0).floor
         end
         ret << (1..l).map{|i| (n >> 40 - i * 8) & 0xff }.pack("c*")
-        off += 8
       end
       return ret
     end
--- test/test_base32.rb
+++ test/test_base32.rb
@@ -2,8 +2,8 @@ require "test/unit"
 require "otp/base32"
 
 class TestBase32 < Test::Unit::TestCase
-  def assert_encode(encoded, plain)
-    assert_equal(encoded, ::OTP::Base32.encode(plain))
+  def assert_encode(encoded, plain, padding: true)
+    assert_equal(encoded, ::OTP::Base32.encode(plain, padding: padding))
   end
 
   def assert_decode(plain, encoded)
@@ -11,9 +11,9 @@ class TestBase32 < Test::Unit::TestCase
     assert_equal(plain, ::OTP::Base32.decode(encoded))
   end
 
-  def assert_encode_decode(plain, encoded)
+  def assert_encode_decode(plain, encoded, padding: true)
     assert_decode(plain, encoded)
-    assert_encode(encoded, plain)
+    assert_encode(encoded, plain, padding: padding)
   end
 
   def test_base32
@@ -26,6 +26,18 @@ class TestBase32 < Test::Unit::TestCase
     assert_encode_decode("fooba", "MZXW6YTB")
     assert_encode_decode("foobar", "MZXW6YTBOI======")
     assert_encode_decode("\u{3042}\u{3044}\u{3046}\u{3048}\u{304a}", "4OAYFY4BQTRYDBXDQGEOHAMK")
+  end
+
+  def test_base32_without_padding
+    assert_encode_decode(nil, nil, padding: false)
+    assert_encode_decode("", "", padding: false)
+    assert_encode_decode("f", "MY", padding: false)
+    assert_encode_decode("fo", "MZXQ", padding: false)
+    assert_encode_decode("foo", "MZXW6", padding: false)
+    assert_encode_decode("foob", "MZXW6YQ", padding: false)
+    assert_encode_decode("fooba", "MZXW6YTB", padding: false)
+    assert_encode_decode("foobar", "MZXW6YTBOI", padding: false)
+    assert_encode_decode("\u{3042}\u{3044}\u{3046}\u{3048}\u{304a}", "4OAYFY4BQTRYDBXDQGEOHAMK", padding: false)
   end
 
   def test_truncated_decode
@@ -42,12 +54,15 @@ class TestBase32 < Test::Unit::TestCase
   def test_unspecified
     assert_raise(ArgumentError){ OTP::Base32.decode("MY0") }
     assert_decode("", "=")
-    assert_decode("", "M=======")
-    assert_decode("f", "MZX=====")
-    assert_decode("foo", "MZXW6Y==")
-    assert_decode("", "M")
-    assert_decode("f", "MZX")
-    assert_decode("foo", "MZXW6Y")
+    assert_decode("`", "M=======")
+    assert_decode("fn", "MZX=====")
+    assert_decode("foo`", "MZXW6Y==")
+    assert_decode("`", "M=======G")
+    assert_decode("fn", "MZX=====G")
+    assert_decode("foo`", "MZXW6Y==G")
+    assert_decode("`", "M")
+    assert_decode("fn", "MZX")
+    assert_decode("foo`", "MZXW6Y")
     assert_decode("fooba", "MZXW6YTB===")
     assert_decode("f", "MY=AAAAA")
     assert_decode("fo", "MZXQ=AAA")
